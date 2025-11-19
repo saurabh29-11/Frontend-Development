2f5a3c02403edb109bc87b80670eeaa28ab82a84
saurabh29-11 <saurabh97219974@gmail.com>
2025-11-19 10:10:47 +0530

    assignment  5

---
+++ Assignment 5/Q5.js
@@ -0,0 +1,23 @@
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.showName = function() {
+  console.log(this.name);
+};
+
+function Student(name, branch) {
+  Person.call(this, name);
+  this.branch = branch;
+}
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+Student.prototype.showBranch = function() {
+  console.log(this.branch);
+};
+
+const s1 = new Student("Rohit", "CSE");
+s1.showName();
+s1.showBranch();
